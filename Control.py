135a23c03de3b80512d5eb791a629d16a6f5c2ef
gloups01 <gloria_wanet@ymail.com>
2016-03-16 15:52:41 +0100

    Update Control.py
    ajout de la fonction ajouterContact

--- Control.py
+++ Control.py
@@ -5,14 +5,21 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 import sys
+from View import *
+from Model import *
 
 class Control :
 
-	def __init__(self,view) :
+	def __init__(self,view, model) :
 		self.view = view
+		self.model = model
+		
 
 	def searchLineEdit() :
 		if (view.lineEditSearch.isModified()) :
 			#Glolo a toi de bosser
 			pass
 	
+	def ajoutContact(self):
+		self.view.addButton.mySignal.connect(self.model.ajouter)
+
